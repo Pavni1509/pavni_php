37c0fd8c0e896edc54f9182511ed31702410f133
Pavni1509 <pavnisuri@gmail.com>
2025-01-29 16:16:39 +0530

    Add files via upload

---
+++ Q35 User Defined Function.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Writing PHP functions</title>
+</head>
+<body>
+<?php
+Function writeMessage(){
+echo"You are really a nice person. Have a nice time!";
+}
+writeMessage();
+echo("<br>This program is written by Pavni Suri-0221BCA030");
+?>
+</body>
+</html>
