3f2ec28d1b75af8498b8e3e1ed5dcd656200a3a6
Pavni1509 <pavnisuri@gmail.com>
2025-03-19 12:27:52 +0530

    Add files via upload

---
+++ Q50 string.php
@@ -0,0 +1,7 @@
+<?php
+
+    $my_str = 'World';
+    echo "Hello, $my_str!<br>";
+
+    echo("<br>This program is written by Pavni Suri-0221BC030");
+?>
---
+++ Q53 string.php
@@ -0,0 +1,11 @@
+<?php
+echo str_replace("world","Dolly","Hello world!");
+echo strrev("Hello world!");
+echo strtolower("Hello WORLD!");
+echo strtoupper("Hello WORLD!");
+echo ucfirst("hello world!");
+echo ucwords("hello world!");
+echo strpos("Hello world!","world");
+echo substr("Hello world",6);
+echo("<br>This program is written by Pavni Suri-0221BC030");
+?>
