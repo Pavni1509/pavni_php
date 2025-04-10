cdae5a0b2e98f8e436e6f9536c10ca90b0fb06c9
Pavni1509 <pavnisuri@gmail.com>
2025-04-10 12:42:51 +0530

    Add files via upload

---
+++ Q64 destroying session.php
@@ -0,0 +1,13 @@
+<?php
+
+    // Starting session
+    session_start();
+     
+    // Removing session variable
+    if(isset($_SESSION["lastname"])){
+        unset($_SESSION["lastname"]);
+    }
+    else{
+        echo "Session already destroyed.";
+    }
+    echo("<br>This program is written by Pavni Suri-0221BC030");
---
+++ Q66 read a file.php
@@ -0,0 +1,11 @@
+<?php
+
+    $file = fopen("welcome.txt", "r") or exit("Unable to open file!");
+  
+    while(!feof($file))
+    {
+        echo fgets($file). "<br>";
+    }
+    fclose($file);
+    echo("<br>This program is written by Pavni Suri-0221BC030");
+?>
